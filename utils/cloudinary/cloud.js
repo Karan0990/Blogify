8aa75284ac62d4ed065134e7068fb02e67fa59be
Karan0990 <75725756+Karan0990@users.noreply.github.com>
2025-07-23 22:27:57 +0530

    Updated cloudinary config

--- utils/cloudinary/cloud.js
+++ utils/cloudinary/cloud.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-    cloud_name: "YOUR_CLOUD_NAME",
-    api_key: "YOUR_API_KEY",
-    api_secret: "YOUR_API_SECRET"
+    cloud_name: "dd0ckmtsp",
+    api_key: "169728499492996",
+    api_secret: "GpJwVIS-1e4QASytTXMXgQwR1Zo"
 });
 
 const storage = new CloudinaryStorage({
