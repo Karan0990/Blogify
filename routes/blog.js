46d129647f48ed51f1fb171c70f4b2b9e2945e1d
Karan0990 <75725756+Karan0990@users.noreply.github.com>
2025-07-23 23:35:40 +0530

    changes

--- routes/blog.js
+++ routes/blog.js
@@ -3,15 +3,15 @@ const { handleCreateBlog, handelBodyOfBlog, handelComment } = require("../contro
 const multer = require("multer");
 const path = require("path")
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.resolve(`./public/uploads/`))
-    },
-    filename: function (req, file, cb) {
-        const fileName = `${Date.now()} + ${file.originalname}`;
-        cb(null, fileName);
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.resolve(`./public/uploads/`))
+//     },
+//     filename: function (req, file, cb) {
+//         const fileName = `${Date.now()} + ${file.originalname}`;
+//         cb(null, fileName);
+//     }
+// })
 
 const { storage } = require("../utils/cloudinary/cloud");
 
